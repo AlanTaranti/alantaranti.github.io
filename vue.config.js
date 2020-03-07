6e02838c8a0906c8d77e1d0e206f81551cbf4dc3
Alan Taranti <alan.taranti@gmail.com>
2020-03-07 03:48:47 -0400

    adicionado purge css

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+const merge = require("webpack-merge");
+const purgeCssConfig = require("./purgecss.config");
+
+const vueConfig = {
   transpileDependencies: ["vuetify"]
 };
+
+module.exports = merge(purgeCssConfig, vueConfig);
