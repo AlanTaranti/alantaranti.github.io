baff597e40c420f0bca9e85947c96eff51fb289d
Alan Taranti <alan.taranti@gmail.com>
2021-08-09 23:08:12 -0400

    nova build

--- README.md
+++
@@ -1,7 +0,0 @@
-# Alan Taranti
-
-Olá, sou eu.
-
-Esse é meu site pessoal.
-
-Acesse-o em <a href="https://alantaranti.github.io" target="_blank">alantaranti.github.io</a>
--- build-and-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git checkout develop
-npm run build
-npm run generate
-git add --all
-git commit -m "nova build"
-git push
-git subtree push --prefix dist origin master
